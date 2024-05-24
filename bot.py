b26a2347d0796a4708ba5922ad1af0e707133189
Sayyed Faizan <103510430+SahilFaiz@users.noreply.github.com>
2024-05-24 18:09:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,30 +1,33 @@
 import os
 from pyrogram import Client, filters
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, User, ChatJoinRequest
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message, ChatJoinRequest
 
-pr0fess0r_99=Client(
+pr0fess0r_99 = Client(
     "𝗕𝗼𝘁 𝗦𝘁𝗮𝗿𝘁𝗲𝗱 𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗢𝗽𝘂𝘀𝗧𝗲𝗰𝗵𝘇",
-    bot_token = os.environ["BOT_TOKEN"],
-    api_id = int(os.environ["API_ID"]),
-    api_hash = os.environ["API_HASH"]
+    bot_token=os.environ["BOT_TOKEN"],
+    api_id=int(os.environ["API_ID"]),
+    api_hash=os.environ["API_HASH"]
 )
 
-CHAT_ID=int(os.environ.get("CHAT_ID", None))
-TEXT=os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
+# Split the CHAT_ID environment variable into a list of integers
+CHAT_IDS = [int(chat_id) for chat_id in os.environ.get("CHAT_ID", "").split(",") if chat_id.isdigit()]
+TEXT = os.environ.get("APPROVED_WELCOME_TEXT", "Hello {mention}\nWelcome To {title}\n\nYour Auto Approved")
 APPROVED = os.environ.get("APPROVED_WELCOME", "on").lower()
 
 @pr0fess0r_99.on_message(filters.private & filters.command(["start"]))
 async def start(client: pr0fess0r_99, message: Message):
-    approvedbot = await client.get_me() 
-    await message.reply_text(text="**𝙷𝙴𝙻𝙻𝙾...WELCOME TO THE BOT !!!")
+    await message.reply_text(text="**𝙷𝙴𝙻𝙻𝙾...WELCOME TO THE BOT !!!**")
 
-@pr0fess0r_99.on_chat_join_request(filters.chat(CHAT_ID))
+@pr0fess0r_99.on_chat_join_request(filters.chat(CHAT_IDS))
 async def autoapprove(client: pr0fess0r_99, message: ChatJoinRequest):
-    chat=message.chat # Chat
-    user=message.from_user # User
-    print(f"{user.first_name} 𝙹𝙾𝙸𝙽𝙴𝙳 ⚡") # Logs
+    chat = message.chat  # Chat
+    user = message.from_user  # User
+    print(f"{user.first_name} 𝙹𝙾𝙸𝙽𝙴𝙳 ⚡")  # Logs
     await client.approve_chat_join_request(chat_id=chat.id, user_id=user.id)
-    
+
+    if APPROVED == "on":
+        welcome_text = TEXT.format(mention=user.mention, title=chat.title)
+        await client.send_message(chat_id=chat.id, text=welcome_text)
 
 print("𝗕𝗼𝘁 𝗦𝘁𝗮𝗿𝘁𝗲𝗱 𝗣𝗹𝗲𝗮𝘀𝗲 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗢𝗽𝘂𝘀𝗧𝗲𝗰𝗵𝘇")
 pr0fess0r_99.run()
